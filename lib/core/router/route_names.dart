c729bdf7823e29a8873dded5abd1ecfdac3f3a49
Ashish Rai <ashish.rai@appinventiv.com>
2025-06-15 09:57:07 +0530

    Route name added

---
+++ lib/core/router/route_names.dart
@@ -0,0 +1,8 @@
+class RouteName {
+  static const String login = '/lgingPage';
+  static const String registration = '/registration';
+  static const String template = '/templeteView';
+
+  // Private constructor to prevent instantiation
+  RouteName._();
+}
