7ea350f40f86276072b1b723b51fa1c0f877cbae
Shubham Rai <shubham.rai@w3villa.com>
2025-06-15 15:05:24 +0530

    worked on three no. templete

--- lib/features/templete/presentation/widget/templete/template_three.dart
+++ lib/features/templete/presentation/widget/templete/template_three.dart
@@ -24,16 +24,102 @@ class TemplateThree extends StatelessWidget {
   final String imagePath;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(color: Colors.purple.shade50),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return
+      Container(
+        padding: const EdgeInsets.all(0),
+        decoration: BoxDecoration(color: Colors.green.shade50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              'asset/images/templete_three_head.png',
+              fit: BoxFit.cover,
+              height: 140,
+              width: double.infinity,
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: BoxDecoration(
+                    border: Border.all(color:  Color(0xff557eb0),width: 4),
+                    // color: Colors.red,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    image: DecorationImage(
+                      image: AssetImage(imagePath),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text(
+                      name,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                    Text(
+                      role,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    infoRow("ID NO", id),
+                    infoRow("DOB", dob),
+                    infoRow("Blood", blood),
+                    infoRow("Phone", phone),
+                    infoRow("E-mail", email),
+                    //  const Spacer(),
+                    // Image.asset(
+                    //   'assets/images/barcode.png', // placeholder for barcode
+                    //   height: 40,
+                    // ),
+                    //  const SizedBox(height: 8),
+                  ],
+                ),
+              ],
+            ),
+            Image.asset(
+              'asset/images/templete_three_bottom.png',
+              fit: BoxFit.fill,
+              height: 50,
+            ),
+            // SizedBox(height: 8),
+            // Text("Name: $name", style: const TextStyle(fontSize: 16)),
+            //  Text("Role: $role", style: const TextStyle(fontSize: 16)),
+          ],
+        ),
+      );
+  }
+  Widget infoRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2,horizontal: 35),
+      child: Row(
         children: [
-          Icon(Icons.business_center, size: 40, color: Colors.purple),
-          SizedBox(height: 8),
-          Text("Name: $name", style: const TextStyle(fontSize: 16)),
-            Text("Role: $role", style: const TextStyle(fontSize: 16)),
+          Expanded(
+            flex: 3,
+            child: Text(
+              "$label:",
+              style: const TextStyle(
+                color: Colors.teal,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Text(
+              value,
+              style: const TextStyle(color: Colors.black87),
+            ),
+          ),
         ],
       ),
     );
