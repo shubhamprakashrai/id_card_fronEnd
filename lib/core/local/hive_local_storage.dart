a3b500279048203d3b8c8ba714ca5bac6db30582
Ashish Rai <ashish.rai@appinventiv.com>
2025-06-15 14:56:17 +0530

    Refactor HiveStorage to manage user and employee data with dedicated Hive boxes

--- lib/core/local/hive_local_storage.dart
+++ lib/core/local/hive_local_storage.dart
@@ -1,50 +1,70 @@
-import 'dart:convert';
+// lib/core/service/hive_storage.dart
+import 'dart:async';
 
-import 'package:hive/hive.dart';
-import 'package:id_card_front_end/core/local/local_Storage_base.dart';
-import 'package:id_card_front_end/features/signup/data/models/signup_response_model.dart';
-import 'package:injectable/injectable.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:id_card_front_end/features/data_scrapper/domain/entities/employee_hive_model.dart';
+import 'package:id_card_front_end/features/data_scrapper/domain/entities/user_hive_model.dart';
 
-@LazySingleton(as: LocalStorage)
-class HiveStorage extends  LocalStorage{
-  static const String _boxName='authBox';
-  static const String _tokenKey='token';
-  static const String _userKey='user';
+class HiveStorage {
+  static const String _authBoxName = 'auth_box';
+  static const String _employeeBoxName = 'employee_box';
+  static const String _userBoxName = 'user_box';
 
-  Box<String> get  _box=>Hive.box(_boxName);
+  late final Box _authBox;
+  late final Box<EmployeeHiveModel> _employeeBox;
+  late final Box<UserHiveModel> _userBox; // Add this line
 
-   @override
-  Future<void> savedToken(String token) async{
-    await _box.put(_tokenKey, token);
+  static late final HiveStorage instance;
+
+  static Future<void> init() async {
+    instance = HiveStorage._();
+    await instance._initHive();
   }
 
-  @override
-  Future<String?> getToken()async {
-   return _box.get(_tokenKey);
+  HiveStorage._();
+
+  Future<void> _initHive() async {
+    _registerAdapters();
+    await _openBoxes(); // Add this line
   }
 
-   @override
-  Future<void> saveUser(UserModel user)async {
-    final jsonString=jsonEncode(user);
-    await _box.put(_userKey,jsonString);
+  FutureOr<void> _openBoxes() async {
+    _authBox = await Hive.openBox(_authBoxName);
+    _employeeBox = await Hive.openBox<EmployeeHiveModel>(_employeeBoxName);
+    _userBox = await Hive.openBox<UserHiveModel>(_userBoxName); // Add this line
   }
 
-
-  @override
-  Future<UserModel?> getUser() async{
-   final jsonString=_box.get(_userKey);
-  if(jsonString==null){
-  return null;
-  }
-  return UserModel.fromJson(jsonDecode(jsonString));
+  void _registerAdapters() {
+    Hive.registerAdapter(EmployeeHiveModelAdapter());
+    Hive.registerAdapter(UserHiveModelAdapter());
   }
 
 
- 
-  @override
-  Future<void> clearStorage()async {
-    _box.clear();
+
+  Future<void> saveUser(UserHiveModel user) => _userBox.put('current_user', user);
+  UserHiveModel? getCurrentUser() => _userBox.get('current_user');
+  Future<void> clearUser() => _userBox.clear();
+
+  // Auth box methods
+  Future<void> setAuthToken(String token) => _authBox.put('token', token);
+  String? get authToken => _authBox.get('token');
+  Future<void> clearAuth() => _authBox.clear();
+
+  // Employee box methods
+  Future<void> saveEmployee(EmployeeHiveModel employee) => _employeeBox.add(employee);
+  Future<void> saveEmployees(List<EmployeeHiveModel> employees) => _employeeBox.addAll(employees);
+  List<EmployeeHiveModel> getAllEmployees() => _employeeBox.values.toList();
+  Future<void> clearEmployees() => _employeeBox.clear();
+
+  Future<void> clearAll() async {
+    await clearAuth();
+    await clearUser();
+    await clearEmployees();
   }
 
-  
+  Future<void> dispose() async {
+    await _authBox.close();
+    await _employeeBox.close();
+    await _userBox.close();
+  }
 }
